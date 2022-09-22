090c322aa51d7f4bc609c80123c97178e2e53572
xzp911 <114185159+xzp911@users.noreply.github.com>
2022-09-23 03:49:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: " uzqd9kFpoxIjAYMKqltswnR4YOplmBZvKGeHUOKorBY ",
+      useTemplateId: "oNefH5ibW_BYuketNmYSM6Aw0IeQ",
       // 所在省份
       province: "安徽",
       // 所在城市
