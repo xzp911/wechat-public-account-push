4c45209713e38c17f835196dfaad97a7fd4c633b
xzp911 <114185159+xzp911@users.noreply.github.com>
2022-09-23 12:41:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wxa6dd88bbbbff4924 ",
+  APP_ID: "wxa6dd88bbbbff4924",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
@@ -55,14 +55,14 @@ export const USER_CONFIG = {
   
   
   /**
-   * 接收公众号消息的微信号，如果有多个，需要在[oNefH5ibW_BYuketNmYSM6Aw0IeQ]里用英文逗号间隔
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
   USERS: [
     {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oNefH5ibW_BYuketNmYSM6Aw0IeQ",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "uzqd9kFpoxIjAYMKqltswnR4YOplmBZvKGeHUOKorBY",
       // 所在省份
