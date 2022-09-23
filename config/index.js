334bc0da20ef7ef255fdbf2e31f960b0d0acf96c
xzp911 <114185159+xzp911@users.noreply.github.com>
2022-09-23 12:15:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ export const USER_CONFIG = {
   
   
   /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+   * 接收公众号消息的微信号，如果有多个，需要在[oNefH5ibW_BYuketNmYSM6Aw0IeQ]里用英文逗号间隔
    */
   USERS: [
     {
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "oNefH5ibW_BYuketNmYSM6Aw0IeQ",
+      useTemplateId: "uzqd9kFpoxIjAYMKqltswnR4YOplmBZvKGeHUOKorBY",
       // 所在省份
       province: "安徽",
       // 所在城市
